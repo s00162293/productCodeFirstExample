babfca6a7001dec4c14cb9ec86ee544a76c2d766
rubabramzan96 <rubabramzan96@gmail.com>
2017-12-12 20:49:01 +0000

    part 2 initial migration added

---
+++ Products/Migrations/ProductsMigrations/201712122044313_initial migrations.cs
@@ -0,0 +1,16 @@
+namespace Products.Migrations.ProductsMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialmigrations : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Products/Migrations/ProductsMigrations/Configuration.cs
@@ -0,0 +1,34 @@
+namespace Products.Migrations.ProductsMigrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Products.Models.ProductDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\ProductsMigrations";
+        }
+
+        protected override void Seed(Products.Models.ProductDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+
+        
+    }
+}
